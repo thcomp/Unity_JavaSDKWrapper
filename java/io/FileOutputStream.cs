f7690f07085338df151522b73ee813886da367ae
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2017-09-15 08:51:03 +0900

    新規作成

---
+++ java/io/FileOutputStream.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace java.io
+{
+    public class FileOutputStream : OutputStream
+    {
+        public FileOutputStream(String name)
+        {
+            mAndroidJO = new AndroidJavaObject("java.io.FileOutputStream", name);
+        }
+
+        public FileOutputStream(String name, Boolean append)
+        {
+            mAndroidJO = new AndroidJavaObject("java.io.FileOutputStream", name, append);
+        }
+    }
+}
---
+++ java/io/OutputStream.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace java.io
+{
+    abstract public class OutputStream
+    {
+        internal AndroidJavaObject mAndroidJO;
+
+        public AndroidJavaObject AndroidJO
+        {
+            get
+            {
+                return mAndroidJO;
+            }
+        }
+
+        public void Close()
+        {
+            mAndroidJO.Call("close");
+        }
+
+        public void Flush()
+        {
+            mAndroidJO.Call("flush");
+        }
+
+        public void Write(byte[] bytes)
+        {
+            mAndroidJO.Call("write", bytes);
+        }
+
+        public void Write(byte[] bytes, int off, int len)
+        {
+            mAndroidJO.Call("write", bytes, off, len);
+        }
+
+        public void Write(int data)
+        {
+            mAndroidJO.Call("write", data);
+        }
+    }
+}
